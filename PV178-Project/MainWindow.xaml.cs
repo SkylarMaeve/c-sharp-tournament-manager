48ae52ad01f5195a27024a133940a283c2505a3f
Nikol Otáhalů <xotahal2@fi.muni.cz>
2025-04-23 15:06:10 +0200

    feat: Baisic sidebar

--- PV178-Project/MainWindow.xaml.cs
+++ PV178-Project/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PV178_Project.Views;
 
 namespace PV178_Project;
 
@@ -19,5 +20,31 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        MainFrame.Navigate(new WelcomePage());
+    }
+
+    private void NavigateToSettings(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(new TournamentSettingsPage());
+    }
+
+    private void NavigateToRanking(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(new RankingPage());
+    }
+
+    private void NavigateToGamePlan(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(new GamePlanPage());
+    }
+
+    private void NavigateToTeams(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(new TeamsPage());
+    }
+
+    private void NavigateToPlayers(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(new PlayersPage());
     }
 }
