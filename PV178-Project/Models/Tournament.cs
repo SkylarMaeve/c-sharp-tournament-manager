3e6d2d9c70a7d01b6cc51df9a9d8b9050a37c5d4
Nikol Otáhalů <xotahal2@fi.muni.cz>
2025-04-26 22:27:30 +0200

    fix: Fix Tournament Data Model

--- PV178-Project/Models/Tournament.cs
+++ PV178-Project/Models/Tournament.cs
@@ -10,16 +10,24 @@ public class Tournament(
     Format format,
     DateTime startDate,
     DateTime endDate,
+    int groups,
     int pointsWin,
     int pointsDraw,
     int pointsLoss
-    ) : BaseModel(id)
+) : BaseModel(id)
 {
     public string Name { get; set; } = name;
     public Sport Sport { get; set; } = sport;
     public Format Format { get; set; } = format;
     public DateTime Start { get; set; } = startDate;
     public DateTime End { get; set; } = endDate;
+    public int GroupsCount { get; set; } = groups;
+
+    public List<string> Groups { get; set; } = Enumerable
+        .Range(0, groups)
+        .Select(i => ((char)('A' + i)).ToString())
+        .ToList();
+
     public int PointsWin { get; set; } = pointsWin;
     public int PointsDraw { get; set; } = pointsDraw;
     public int PointsLoss { get; set; } = pointsLoss;
--- PV178-Project/Services/DataProvider.cs
+++ PV178-Project/Services/DataProvider.cs
@@ -10,7 +10,7 @@ public class DataProvider()
     public BaseManagerModel<Match> Matches { get; private set; } = new BaseManagerModel<Match>();
     public BaseManagerModel<Tournament> Tournaments { get; private set; } = new BaseManagerModel<Tournament>();
     public BaseManagerModel<Sport> Sports { get; private set; } = new BaseManagerModel<Sport>();
-    
+
     //Initializes Managers
     public void Initialize()
     {
@@ -19,9 +19,10 @@ public class DataProvider()
         SetTeams();
         SetPlayers();
     }
+
     private void SetSports()
     {
-        var data =  new List<Sport>
+        var data = new List<Sport>
         {
             new Sport(0, "Hockey", 60),
             new Sport(1, "FImon", 60),
@@ -32,28 +33,30 @@ public class DataProvider()
             Sports.Add(value);
         }
     }
+
     private void SetTeams()
     {
-        var data =  new List<Team>
+        var data = new List<Team>
         {
-            new Team(0,"Team A",Tournaments.GetData()[0], "A"),
-            new Team(1,"Team B",Tournaments.GetData()[0], "A"),
-            new Team(2,"Team C",Tournaments.GetData()[0], "A"),
+            new Team(0, "Team A", Tournaments.GetData()[0], "A"),
+            new Team(1, "Team B", Tournaments.GetData()[0], "A"),
+            new Team(2, "Team C", Tournaments.GetData()[0], "A"),
         };
         foreach (var value in data)
         {
             Teams.Add(value);
         }
     }
+
     private void SetPlayers()
     {
-         var data = new List<Player>
+        var data = new List<Player>
         {
-            new Player(0,"John 1", Teams.GetData()[0], new DateTime(2002, 12, 1)),
-            new Player(1,"John 2", Teams.GetData()[0], new DateTime(2002, 12, 1)),
-            new Player(2,"John 3", Teams.GetData()[1], new DateTime(2002, 12, 1)),
-            new Player(3,"John 4", Teams.GetData()[1], new DateTime(2002, 12, 1)),
-            new Player(4,"John 5", Teams.GetData()[2], new DateTime(2002, 12, 1)),
+            new Player(0, "John 1", Teams.GetData()[0], new DateTime(2002, 12, 1)),
+            new Player(1, "John 2", Teams.GetData()[0], new DateTime(2002, 12, 1)),
+            new Player(2, "John 3", Teams.GetData()[1], new DateTime(2002, 12, 1)),
+            new Player(3, "John 4", Teams.GetData()[1], new DateTime(2002, 12, 1)),
+            new Player(4, "John 5", Teams.GetData()[2], new DateTime(2002, 12, 1)),
         };
         foreach (var value in data)
         {
@@ -63,18 +66,22 @@ public class DataProvider()
 
     private void SetTournaments()
     {
-        var data =  new List<Tournament>
+        var data = new List<Tournament>
         {
-            new Tournament(0, "FImonWorld CHampionship",  Sports.GetData()[0], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 2, 1, 0),
-            new Tournament(1, "Dunno",  Sports.GetData()[0], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 2, 1, 0),
-            new Tournament(2, "Dunno",  Sports.GetData()[1], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 2, 1, 0),
-            new Tournament(3, "Dunno",  Sports.GetData()[2], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 2, 1, 0),
-            new Tournament(4, "Dunno",  Sports.GetData()[1], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 2, 1, 0),
+            new Tournament(0, "FImonWorld CHampionship", Sports.GetData()[0], Format.GroupsAndPlayOff, DateTime.Now,
+                DateTime.Now, 3, 2, 1, 0),
+            new Tournament(1, "Dunno", Sports.GetData()[0], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 3, 2,
+                1, 0),
+            new Tournament(2, "Dunno", Sports.GetData()[1], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 3, 2,
+                1, 0),
+            new Tournament(3, "Dunno", Sports.GetData()[2], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 3, 2,
+                1, 0),
+            new Tournament(4, "Dunno", Sports.GetData()[1], Format.GroupsAndPlayOff, DateTime.Now, DateTime.Now, 3, 2,
+                1, 0),
         };
         foreach (var value in data)
         {
             Tournaments.Add(value);
         }
-
     }
 }
